7dc1016d05d12ba1a416cb697b36ac5b82462f85
maggiechn20 <maggiemc.chen@mail.utoronto.ca>
2022-03-29 18:28:44 -0400

    added beginning land portion

--- frogger.asm
+++ frogger.asm
@@ -13,7 +13,7 @@ displayAddress: .word 0x10008000
 .text
 lw $t0, displayAddress # $t0 stores the base address for display
 li $t1, 0xff0000 # $t1 stores the red colour code
-li $t2, 0x00ff00 # $t2 stores the green colour code
+li $t2, 0x4dad53 # $t2 stores the green colour code
 li $t3, 0x0000ff # $t3 stores the blue colour code
 sw $t1, 0($t0) # paint the first (top-left) unit red.
 sw $t2, 4($t0) # paint the second unit on the first row green. Why $t0+4?
@@ -46,9 +46,9 @@ addi $t5, $t5, 384		# set $t5 to 640. ($t5 was last 256, and 640 = 258 + 384)
 addi $t0, $t0, 1024		# set $t0 to the new beginning pixel
 
 draw_middle:
-beq $t4, $t5, Exit		# Branch to Exit if $t4 == 640
-sw $t2, 0($t0)			# paint the first unit on the first row green. 
-addi $t0, $t0, 4		# move to the next pixel in the bitmap
+beq $t4, $t5, start_beginning_draw	# Branch to Exit if $t4 == 640
+sw $t2, 0($t0)				# paint the first unit on the first row green. 
+addi $t0, $t0, 4			# move to the next pixel in the bitmap
 
 addi $t4, $t4, 1		# incrment $t4 by 1
 j draw_middle
@@ -58,6 +58,18 @@ j draw_middle
 
 
 # draw the starting green land 
+start_beginning_draw:
+addi $t4, $t4, 256		# set $t4 to 896. Since $t4 was last 640, 896 = 640 + 256 
+addi $t5, $t5, 384		# set $t5 to 1024. ($t5 was last 640, and 1024 = 640 + 384)
+addi $t0, $t0, 1024		# set $t0 to the new beginning pixel
+
+draw_beginning:
+beq $t4, $t5, Exit		# Branch to Exit if $t4 == 640
+sw $t2, 0($t0)			# paint the first unit on the first row green. 
+addi $t0, $t0, 4		# move to the next pixel in the bitmap
+
+addi $t4, $t4, 1		# incrment $t4 by 1
+j draw_beginning
 
 Exit:
 li $v0, 10 # terminate the program gracefully
