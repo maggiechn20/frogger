bbdb703b45e6e5d22a26c9cdbdf67014c51c97d4
maggiechn20 <maggiemc.chen@mail.utoronto.ca>
2022-04-02 02:29:58 -0400

    have index shift code, just in case

--- frogger1.asm
+++ frogger1.asm
@@ -42,9 +42,17 @@ vehicles_1: 	.space 512			# 4*32*4 bytes allocated for the first road
 vehicles_2:     .space 512			# 4*32*4 bytes allocated for the second road
 river_1: 	.space 512			# 4*32*4 bytes allocated for the first river
 river_2:     	.space 512			# 4*32*4 bytes allocated for the second river
+car1_x:		.word 0 
+car2_x: 	.word 16
+car3_x:		.word 4
+car4_x:		.word 20
+log1_x:		.word 0
+log2_x:		.word 16
+log3_x:		.word 4
+log4_x:		.word 20
 
 .text
-
+repaint:
 
 ###### DRAW SAFE SPACE LOCATIONS ####### 
 
@@ -119,6 +127,7 @@ skip_draw_safe_space_function:	# Skip the function
 # Set parameters for allocate_memory
 addi $a2, $zero, 0 	# set the x location of the first car 
 addi $a3, $zero, 16 	# set the x location of the second car
+
 
 li $t7, 0x808080 	# $t7 stores the grey colour used for the road 
 li $t8, 0xffff00	# $t8 stores the yellow colour for cars 
@@ -224,6 +233,8 @@ allocate_memory:
 	# Check if $t2 <= $a2 + 8
 	double_check_range_1:
 	addi $t5, $a2, 8			# add 8 to the x position of car, getting the full width of the car1
+	
+	# bge $t5, 32, overflow_control		# If the car drawing overflows out of display, go to overflow_control
 	ble $t2, $t5, colour_yellow 		# if $t2 <= $a2 + 8, that means it is a car1 pixel.
 	j check_next				# if $t2 >= $a2 + 8, check if it passes the x position of car2
 
@@ -245,17 +256,22 @@ allocate_memory:
 	sw $t7, 0($t4) 		   		# assign grey (A[i] = 0x808080)
 
 	done_colouring:	
-	   				   			  		   			   			   			   			   			   				   			  		   			   			   			   			   		   				   			  		   			   			   			   			   			   				   			  		   			   			   			   			   
+   			  		   			   			   			   			   			   				   			  		   			   			   			   			   		   				   			  		   			   			   			   			   			   				   			  		   			   			   			   			   
 	addi $t2, $t2, 1 	   		# increment width by 1 
 	addi $t6, $t6, 1			# increment array counter by 1
 	j draw_obstacle_line_loop	   	
 	end_draw_obstacle_line:			# Finish drawing line
+	
+
 
 	addi $t1, $t1, 1			# Increment $t1 （height）by 1
 	j draw_obstacle_rectangle_loop		#  Jump to start of rectangle drawing loop
 
 	done_obstacle_draw:			# When $t1 == height ($a0), the drawing is done.
 jr $ra
+
+
+
 	
 
 # FUNCTION: paint all the pixels in the .space array 
@@ -308,6 +324,63 @@ sw $t4, 4($t0) 		#
 addi $t0, $t0, 124 	# Lower legs of frog 
 sw $t4, 0($t0) 		# 
 sw $t4, 12($t0) 	# 
+
+
+
+### Increment the positions of the cars and logs for the next repaint ### 
+
+### Shift ###
+la $t9, vehicles_1			#load the address of the memory space
+
+addi $s0, $t9, 0		# Find the address of the first pixel of one row
+lw $s1, 0($s0)			# load the value of $s0 into register $s1
+
+add $t1, $zero, $zero 		# Set index value ($t1) to zero. This will be index i for storing into the array.
+sll $t2, $t1, 2 	   	# $t2 = $t1 * 4 = i * 4 = offset 
+add $t3, $t9, $t2 	   	# $t3 = addr(A) + i * 4 = addr(A[i]) 
+
+# $a0 and $a1 are the height and the width of the river/road, respectively 
+addi $a0, $zero, 4		# set height = 4
+addi $a1, $zero, 32		# set width = 32
+
+# Draw a rectangle:
+add $t4, $zero, $zero		# Set index value ($t4) to zero
+draw_rect_loop_shift:
+beq $t4, $a0, exit_shift  	# If $t4 == height ($a0), jump to end
+
+# Draw a line:
+add $t5, $zero, $zero		# Set index value ($t5) to zero
+draw_line_loop_shift:
+beq $t5, $a1, end_draw_line_shift  	# If $t5 == width ($a1), jump to end
+
+# Store the value of the next pixel
+addi $t6, $t1, 1		# -> Store the value of index $t1, incremented by one, to another register ($t6)
+sll $t6, $t6, 2			# Offset
+add $t7, $t9, $t6		# -> Add this to $t9 store to register $t7
+lw $t8, 0($t7)			# -> Load word from $t7 to $t8
+sw $t8, 0($t3)			# -> Store this word into $t3
+
+# Store the value of the first pixel to the last pixel
+sw $s1, 0($t3)
+
+addi $t5, $t5, 1 		# Increment index ($t5)
+addi $t1, $t1, 1		# increment array counter by 1
+
+j draw_line_loop_shift	#   - Jump to start of line drawing loop
+end_draw_line_shift:
+
+addi $t4, $t4, 1	#   - Increment $t6 by 1
+j draw_rect_loop_shift	#   - Jump to start of rectangle drawing loop
+ 
+exit_shift:
+
+### Sleep ###
+li $v0, 32
+li $a0, 200
+syscall
+
+
+j repaint		# Loop up to the very top again for repainting
 
 
 Exit:
